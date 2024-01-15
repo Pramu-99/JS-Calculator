41c76bde0b50e07a7a7ea56006baf91e9026e83a
Pramu-99 <pramuxjaya99@gmail.com>
2024-01-16 00:26:53 +0530

    fisrt commit

---
+++ index.js
@@ -0,0 +1,21 @@
+//calculator functionality
+const display = document.getElementById("display");
+
+//function to display items
+function appendToDisplay(input) {
+    display.value += input;
+}
+
+//function to calculate
+function clearDisplay() {
+    display.value = "";
+}
+
+//function to clear out the display
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch (e) {
+        display.value = "error";
+    }
+}
